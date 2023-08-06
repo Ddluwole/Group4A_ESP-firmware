d3bf44deacf5a5cc2dbd32c7f1c4b2912fe2eeb1
David Ilori <92074984+Ddluwole@users.noreply.github.com>
2023-08-06 21:28:04 +0100

    Create final_code.ino

---
+++ final_code.ino
@@ -0,0 +1,51 @@
+#include <HardwareSerial.h>
+
+// Configure DAC pins
+const int dac1 = 25;
+const int dac2 = 26;
+
+
+void setup() {
+  // Initialize baud rate for serial communication
+  Serial.begin(9600);
+
+  // Configure DAC pins
+  pinMode(dac1, OUTPUT);
+  pinMode(dac2, OUTPUT);
+}
+
+void loop() {
+  // Wait for serial input
+  while (Serial.available() < 1) {
+    delay(500);
+  }
+
+  // Read serial input and remove leading/trailing whitespaces
+  String input_values = Serial.readStringUntil('\n');
+  input_values.trim();
+
+  // Find the position of the comma in the input string
+  int comma_index = input_values.indexOf(",");
+  if (comma_index < 0) {
+  // If no comma found, exit the loop and wait for the next input
+    return; 
+  }
+
+  // Extract the substrings before and after the comma
+  String digital_value1 = input_values.substring(0, comma_index);
+  String digital_value2 = input_values.substring(comma_index + 1);
+
+  // Convert the floating-point values to voltages
+
+  float digital_value1 = digital_value1.toFloat();
+  float digital_value2 = digital_value2.toFloat();
+
+  // Convert the floating-point values to voltages
+  float first_voltage = (digital_value1 / 255.0) * 3.3;
+  float second_voltage = (digital_value2 / 255.0) * 3.3;
+
+  // Output voltages to DAC pins (converting voltage to 8-bit DAC value)
+  dacWrite(dac1, (int)((first_voltage / 3.3) * 255));
+  dacWrite(dac2, (int)((second_voltage / 3.3) * 255));
+
+}
